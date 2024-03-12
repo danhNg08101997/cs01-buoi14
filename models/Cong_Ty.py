278656d4cd3dca2a3b884a919c0f8032d6d86d54
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-03-12 23:42:11 +0700

    oop buoi 1

--- models/Cong_Ty.py
+++ models/Cong_Ty.py
@@ -1,6 +1,18 @@
-from Nhan_Vien import Nhan_Vien
+# from Nhan_Vien import Nhan_Vien
+from models.Nhan_Vien import Nhan_Vien
 
 class Cong_Ty:
     def __init__(self) -> None:
         self.ds_nhan_vien: list[Nhan_Vien] = []
         self.ten_cong_ty = ''
+    def them_nhan_vien(self, nhan_vien_moi:Nhan_Vien):
+        self.ds_nhan_vien.append(nhan_vien_moi)
+    def xuat_thong_tin_nhan_vien(self):
+        for nv in self.ds_nhan_vien:
+            nv.xuat_thong_tin()
+    def xoa_nhan_vien(self, ma_nhan_vien:str):
+        for nv in self.ds_nhan_vien:
+            if nv.ma_nhan_vien == ma_nhan_vien:
+                self.ds_nhan_vien.remove(nv)
+        # Gọi lại phương thức hiển thị sau khi xóa
+        self.xuat_thong_tin_nhan_vien()
--- quan_ly_nhan_vien.py
+++ quan_ly_nhan_vien.py
@@ -1,1 +1,29 @@
 # Yêu cầu: Viết chương trình tính lương và quản lý thông tin nhân viên bao gồm: ma_nhan_vien, ten_nhan_vien, so_dien_thoai, email, so_gio_lam_viec, luong_cb cho 1 công ty A.
+
+from models.Cong_Ty import Cong_Ty
+from models.Nhan_Vien import Nhan_Vien
+
+cong_ty = Cong_Ty()
+# Thiết kế menu
+running = True
+while running:
+    print(f'''
+          1/ Thêm nhân viên
+          2/ Xóa nhân viên
+          3/ Xuất thông tin nhân viên
+          Bấm e/E để thoát chương trình
+          ''')
+    select = input('Chọn chức năng: ')
+    if select == '1':
+        nv = Nhan_Vien()
+        nv.nhap_thong_tin()
+        nv.xuat_thong_tin()
+        # Thêm nhân viên vào công ty
+        cong_ty.them_nhan_vien(nv)
+    elif select == '2':
+        ma_nhan_vien = input('Nhập vào mã nhân viên cần xóa: ')
+        cong_ty.xoa_nhan_vien(ma_nhan_vien)
+    elif select == '3':
+        cong_ty.xuat_thong_tin_nhan_vien()
+    elif select == 'e' or 'E':
+        running = False
